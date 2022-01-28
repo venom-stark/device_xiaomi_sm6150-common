cde3e7986807c5eeccf7f8d2ed4ee51e52daafb4
venom-stark <sachidanandabehera562@gmail.com>
2022-01-28 22:09:45 +0530

    sm6150-common : Declare Product Board and Platform on device makefile
    Signed-off-by: venom-stark <sachidanandabehera562@gmail.com>

--- sm6150.mk
+++ sm6150.mk
@@ -430,14 +430,17 @@ PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
     hardware/xiaomi
 
+PRODUCT_BOARD_PLATFORM := sm6150
+PRODUCT_USES_QCOM_HARDWARE := true
+
 # Telephony
 PRODUCT_PACKAGES += \
     ims-ext-common \
     ims_ext_common.xml \
     qti-telephony-hidl-wrapper \
     qti_telephony_hidl_wrapper.xml \
-		qti-telephony-hidl-wrapper-prd \
-		qti_telephony_hidl_wrapper_prd.xml \
+	qti-telephony-hidl-wrapper-prd \
+	qti_telephony_hidl_wrapper_prd.xml \
     qti-telephony-utils \
     qti_telephony_utils.xml \
     telephony-ext
