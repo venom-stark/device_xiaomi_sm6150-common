a3568ba3e97cc5c70f04e4187316d222e2dac9a0
venom-stark <sachidanandabehera562@gmail.com>
2022-06-17 12:19:34 +0530

    sm6150-common: Inherit GcamGo

--- sm6150.mk
+++ sm6150.mk
@@ -214,6 +214,10 @@ PRODUCT_PACKAGES += \
 # GPS Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.location.gps.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.location.gps.xml
+    
+#GCamgo
+PRODUCT_PACKAGES += \
+    GCamGOPrebuilt
 
 # Health
 PRODUCT_PACKAGES += \
